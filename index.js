e106882194442ccc731e4f923540076beaddd225
Daniel Paul Perinchery <67698207+danielpaul-123@users.noreply.github.com>
2025-11-22 19:48:11 +0530

    added cors support

--- index.js
+++ index.js
@@ -12,7 +12,12 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: ['https://thm.ieee-link.org', 'http://localhost:3000', 'http://localhost:5173'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true,
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
